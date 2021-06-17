5785bae2deb9d4a0ead76b46eadfa4d67d54d903
krumma147 <krumma147@gmail.com>
2021-06-17 20:21:59 +0700

    update app.js 17/06

--- react_learning/my-app/src/App.js
+++ react_learning/my-app/src/App.js
@@ -30,10 +30,10 @@ class App extends Component{
             {
             author : "Minh",
             content : "Video provides a powerful way to help you prove your point. When you click Online Video, you can paste in the embed code for the video you want to add. You can also type a keyword to search online for the video that best fits your document." + 
-            "To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries."+
+            " To make your document look professionally produced, Word provides header, footer, cover page, and text box designs that complement each other. For example, you can add a matching cover page, header, and sidebar. Click Insert and then choose the elements you want from the different galleries."+
             "Themes and styles also help keep your document coordinated. When you click Design and choose a new Theme, the pictures, charts, and SmartArt graphics change to match your new theme. When you apply styles, your headings change to match the new theme."+
-            "Save time in Word with new buttons that show up where you need them. To change the way a picture fits in your document, click it and a button for layout options appears next to it. When you work on a table, click where you want to add a row or a column, and then click the plus sign."+
-            "Reading is easier, too, in the new Reading view. You can collapse parts of the document and focus on the text you want. If you need to stop reading before you reach the end, Word remembers where you left off",
+            " Save time in Word with new buttons that show up where you need them. To change the way a picture fits in your document, click it and a button for layout options appears next to it. When you work on a table, click where you want to add a row or a column, and then click the plus sign."+
+            " Reading is easier, too, in the new Reading view. You can collapse parts of the document and focus on the text you want. If you need to stop reading before you reach the end, Word remembers where you left off.",
             text:'',
             comment : [
                 {
@@ -226,7 +226,7 @@ class App extends Component{
                         <Modal isOpen={modal} toggle={(ev)=>this.toggle(ev, 0)}>
                                     <ModalHeader toggle={(ev)=>this.toggle(ev, 0)}>Modal title</ModalHeader>
                                     <ModalBody>
-                                        <Input type="textarea" className="inputPost" value={this.getPost()} rows={5} />
+                                        <Input type="textarea" className="inputPost" defaultValue={this.getPost()} rows={5} />
                                     </ModalBody>
                                     <ModalFooter>
                                         <Button color="primary" onClick={(ev)=>this.editPost(ev)}>Change</Button>{' '}
